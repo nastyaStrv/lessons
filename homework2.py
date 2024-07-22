b692b50a769752a2ec8a938f1a5558a16793b7a3
nastyaStrv <for.wk@mail.ru>
2024-07-22 12:15:34 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+hw_done_count = 12
+time_spent = 1.5
+name_of_course = 'Python'
+time_for_one_task = (time_spent / hw_done_count)
+print('Курс:', name_of_course, ', всего задач:', hw_done_count, ', затрачено часов:', time_spent,
+      ', среднее время выполнения:', time_for_one_task, 'часа.')
