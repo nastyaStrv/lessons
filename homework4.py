88c4ed91b3312797d38b39942638eb3e02a77c78
nastyaStrv <for.wk@mail.ru>
2024-07-22 16:28:03 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string=input('Произвольный текст: ')
+print('Количество введенных символов = ',(len(my_string)))
+
+print((my_string).upper())
+print((my_string).upper().lower())
+print((my_string).replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
