7d6965b88e8662096ae281a185da6d49ef33a71d
nastyaStrv <for.wk@mail.ru>
2024-08-01 15:47:20 +0300

    Add files via upload

---
+++ module_2_2..py
@@ -0,0 +1,13 @@
+first = 9
+second = 19
+third = 9
+
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+elif first != second and first != third and second != third:
+    print(0)
+
+
+
