1c3256c2127f23f1d67f743e26d7f4fe39dd7f05
nastyaStrv <for.wk@mail.ru>
2024-08-02 12:09:20 +0300

    Add files via upload

---
+++ module_2_3(2).py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+num = 0
+while num < len(my_list):
+    num_2 = my_list[num]
+    num = num+1
+    if num_2 == 0:
+        continue
+    elif num_2 < 0:
+        break
+    else:
+        print(num_2)
