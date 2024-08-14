fb65eec5934cc7edf8b494ea586e433493205e8e
nastyaStrv <for.wk@mail.ru>
2024-08-14 09:20:08 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,22 @@
+from fake_math import divide as div_2
+
+result1 = div_2(69, 3)
+result2 = div_2(3, 0)
+print(result1)
+print(result2)
+
+from true_math import divide as div_1
+
+result3 = div_1(49, 7)
+result4 = div_1(15, 0)
+print(result3)
+print(result4)
+
+
+
+
+
+
+
+
+
