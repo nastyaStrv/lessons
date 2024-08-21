c9facd98e262b4103eb0fe2528efcf41e73a3de7
nastyaStrv <for.wk@mail.ru>
2024-08-21 11:53:59 +0300

    Add files via upload

--- module_4_2.py
+++ module_4_2.py
@@ -1,7 +1,9 @@
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
-    return inner_function()
+    inner_function()
 
-print(test_function())
-print(inner_function())
+
+test_function() #Работает
+inner_function() #NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+                 #Является ошибкой т.к. разные пространства имен. Мы не можем достать функцию которая находится внутри функции.
