2f563b7c44e75d475264247f595576b9b76ed37b
nastyaStrv <for.wk@mail.ru>
2024-09-09 10:31:55 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -9,27 +9,23 @@ class Product:
     def __str__(self):
         return (f'{self.name}, {self.weight}, {self.category}')
 
+
 class Shop:
-    def __init__(self):
-        self.__file_name = 'products.txt'
+    __file_name = 'products.txt'
 
     def get_products(self):
-        self.__file_name = 'products.txt'
         file = open(self.__file_name, 'r')
-        products = file.read()
-        file.close()
-
-        return products
+        return file.read()
 
     def add(self, *products):
+        self.get_products()
         for i in products:
-            if i not in self.__file_name:
+            if self.get_products().find(f'{i.weight}') == -1:
                 file = open(self.__file_name, 'a')
                 file.write(f'{i}\n')
                 file.close()
-        else:
-            print(f'Продукт {i} уже есть в магазине')
-
+            else:
+                print(f'Продукт {i.name} {i.weight} {i.category} уже есть в магазине')
 
 
 s1 = Shop()
@@ -37,12 +33,11 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
-
 
 
 
