730075c791d51dd01e3ca0188f90a2552c6d0bd3
nastyaStrv <for.wk@mail.ru>
2024-11-27 14:58:46 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher, executor, types
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler()
+async def all_message(message: types.Message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
