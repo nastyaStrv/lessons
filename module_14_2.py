e01da756a9d4a2b4b4ca3bbcca57ce3f5c80489a
nastyaStrv <for.wk@mail.ru>
2024-12-06 13:22:44 +0300

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute("DELETE FROM Users WHERE id = 6")
+
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balances = cursor.fetchone()[0]
+
+cursor.execute("SELECT AVG(balance) FROM Users")
+average_balance = cursor.fetchone()[0]
+
+# Код из предыдущего задания
+# Удаление пользователя с id=6
+# Подсчёт кол-ва всех пользователей
+# Подсчёт суммы всех балансов
+print(all_balances / total_users)
+connection.close()
+
