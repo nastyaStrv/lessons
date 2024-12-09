e46fae147f9ccb7b1153cec39c8b6a7574ab68bd
nastyaStrv <for.wk@mail.ru>
2024-12-09 12:38:29 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -9,8 +9,33 @@ def initiate_db():
                       title TEXT NOT NULL,
                       description TEXT,
                       price INTEGER NOT NULL)''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users (
+                      id INTEGER PRIMARY KEY,
+                      username TEXT NOT NULL,
+                      email TEXT NOT NULL,
+                      age INTEGER NOT NULL,
+                      balance INTEGER NOT NULL)''')
+
     connection.commit()
     connection.close()
+
+
+def add_user(username, email, age):
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Users WHERE username=?", (username,))
+    if cursor.fetchone() is not None:
+        cursor.execute(''' INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, 1000) ''',
+                       (username, email, age))
+    connection.commit()
+
+
+def is_included(username):
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Users WHERE username=?", (username,))
+    return cursor.fetchone() is not None
 
 
 def get_all_products():
