83cda8b4bdbc0686fe655cf394118d3cd6474b4f
nastyaStrv <for.wk@mail.ru>
2025-01-10 15:20:06 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Главная страница"}
+
+
+@app.get('/user/admin')
+async def admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+
+@app.get('/user/{user_id}')
+async def user(user_id: int = Path(ge=1, le=100, description='Enter User ID', example=1)) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+
+@app.get('/user/{username}/{age}')
+async def user(username: Annotated[str, Path(min_length=5,
+                                             max_length=20,
+                                             description='Enter username',
+                                             example='UrbanUser')],
+               age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)],):
+    return {f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
+
+# @app.get('/id')
+# async def user(username: str, age: int):
